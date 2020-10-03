03fc4f9750329e8a1531bcd44d190903249f04c3
alexanderalban <albanalexander@gmail.com>
2020-10-03 18:43:01 -0500

    header and footer added to all htmls

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,15 @@
 const whereami = () => {
     document.getElementById("currentnav").style.color = rgb(11,163,156);
 }
+
+const hamburgerclick = () => {
+    element=document.getElementById("hamburger-button")
+    if (document.getElementById("hamburger-button").src = "BarberImages/mobile-menu.png))
+    {
+
+        document.getElementById("hamburger-button").src = "BarberImages/mobile-close.png";
+    }
+    else
+    {
+        document.getElementById("hamburger-button").src = "BarberImages/mobile-menu.png";
+    }
