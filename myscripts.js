95ea4aa9927657bea33492e7102a0b4722b50037
alexanderalban <albanalexander@gmail.com>
2020-10-05 16:21:08 -0500

    blog reactivity

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,3 @@
-const whereami = () => {
-    document.getElementById("currentnav").style.color = rgb(11,163,156);
-}
-
 const hamburgerclick = () => {
     element=document.getElementById("hamburger-button")
     if (document.getElementById("hamburger-button").src = "BarberImages/mobile-menu.png))
