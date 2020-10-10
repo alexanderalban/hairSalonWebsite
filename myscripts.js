35faa1488b5d01f0f5458cd13136182c7802bc98
alexanderalban <albanalexander@gmail.com>
2020-10-10 17:48:26 -0500

    nav fixes, hamburger adjustments

--- myscripts.js
+++ myscripts.js
@@ -1,19 +1,15 @@
-var navbar = document.querySelector(".hamburger-menu")
-var ham = document.querySelector(".hamburger-button")
 
-// toggles hamburger menu in and out when clicking on the hamburger
-function toggleHamburger(){
-  navbar.classList.toggle("showNav")
-  ham.classList.toggle("hamburger-close")
+
+let toggleHamburger = () => {
+    console.log("Hello there");
+
+    const hamburgermenu = document.getElementById("hamburger-menu");
+    if (hamburgermenu.style.display == "none") {
+    hamburgermenu.style.display = "grid";
+    } else {hamburgermenu.style.display = "none";
+        
+    }  
+    
+
+
 }
-
-
-// toggle when clicking on links
-
-// METHOD 1
-var menuLinks = document.querySelectorAll(".menuLink")
-menuLinks.forEach( 
-  function(menuLink) { 
-    menuLink.addEventListener("click", toggleHamburger) 
-  }
-)
