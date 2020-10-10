23c5dc705ab0237e8bcc1d1d337009e5715f85a1
alexanderalban <albanalexander@gmail.com>
2020-10-10 16:45:08 -0500

    hamburger button adjustments

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,19 @@
-let hamburgerclick = () => {
-    
-    console.log("hello there")
+var navbar = document.querySelector(".hamburger-menu")
+var ham = document.querySelector(".hamburger-button")
 
-    if (document.getElementById("hamburger-button").src == "./BarberImages/mobile-menu.png")
-    {
-        document.getElementById("hamburger-button").src = "./BarberImages/mobile-close.png";
-    }
-
-    
+// toggles hamburger menu in and out when clicking on the hamburger
+function toggleHamburger(){
+  navbar.classList.toggle("showNav")
+  ham.classList.toggle("hamburger-close")
 }
+
+
+// toggle when clicking on links
+
+// METHOD 1
+var menuLinks = document.querySelectorAll(".menuLink")
+menuLinks.forEach( 
+  function(menuLink) { 
+    menuLink.addEventListener("click", toggleHamburger) 
+  }
+)
